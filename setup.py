9034fa5c912fe532b8b5089e8d40b4e29bd8a692
yzqin <yzqin1996@gmail.com>
2024-04-11 21:33:36 -0700

    [fix] change isaacgym version to 1.0rc2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author="Yuzhe Qin",
     author_email="y1qin@ucsd.edu",
     description="PEP 561 type stubs for isaacgym.gymapi",
-    version='1.0rc4',
+    version='1.0rc2',
     packages=['isaacgym-stubs'],
     package_data={"isaacgym-stubs": ['gymapi.pyi', '__init__.pyi']},
 )
